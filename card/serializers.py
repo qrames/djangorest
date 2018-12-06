64dd0707f3feb0609fe4bb6a335ead5bffba827f
qrames <ramque@orange.fr>
2018-12-06 11:19:08 +0100

    debug

--- card/serializers.py
+++ card/serializers.py
@@ -1,14 +1,14 @@
+# -*- coding: utf-8 -*-
 # api/serializers.py
 
 
 from rest_framework import serializers
-from blogengine import models
-
+from .models import Card
 
 class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = models.Post
+        model = Card
         # Vous pouvez ajouter un fields pour filtrer les
         # champs du modèle à sérialiser
         fields = ('title', 'content')
